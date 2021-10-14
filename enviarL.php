656c272f38c3698767d730f8e24e5fae8b640ae2
Jairo-Sama <43211034+Jairo-Sama@users.noreply.github.com>
2021-10-14 15:56:06 -0500

    evaluacion 1

---
+++ enviarL.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$app = $_POST['app'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " " .$app . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+
+$para = 'hermanrv04@gmail.com';
+$asunto = 'Mensaje de SUBSCRIPCIONES';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:in.html");
+?>
